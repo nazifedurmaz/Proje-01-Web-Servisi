a6c476b6766afce2f69ead8c5152dd773b4cda88
nazifedurmaz <127083463+nazifedurmaz@users.noreply.github.com>
2026-03-17 01:51:32 +0300

    Use environment variable for server port
    Updated app.run to use environment variable for port.

--- api.py
+++ api.py
@@ -230,4 +230,6 @@ if __name__ == "__main__":
     print("  XSD      :  http://localhost:5000/api/schema.xsd")
     print("  Arayüz   :  http://localhost:5000/")
     print("=" * 55)
-    app.run(debug=True, port=5000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
